6603c1c3f45cc853fcf7972882a8539749f65f0f
alimardon28 <doniyorovalimardon369@gmail.com>
2022-06-01 22:09:38 +0500

    added navbar responsive , news sections

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
+const navbar = document.querySelector(".navbar");
+const hamburgerBtn = document.querySelector(".hamburgerBtn");
+const closeBtn = document.querySelector(".closeBtn");
+
+hamburgerBtn.addEventListener("click", () => {
+  navbar.classList.add("activNavbar");
+  closeBtn.addEventListener("click", () => {
+    navbar.classList.remove("activNavbar");
+  });
+});
+
 // // https://www.daryo.uz/api/news
 
 // const URL = 'https://www.daryo.uz/api/news';
